1398d6dff1a2c58d9a775d12da119b3aff1eb532
p.rogovin <p.rogovin@devinotele.com>
2023-01-31 20:45:14 +0400

    [B1] App is connected to backend.

---
+++ src/store/index.ts
@@ -0,0 +1,14 @@
+import authReducer from './authReducer';
+
+import { configureStore } from '@reduxjs/toolkit';
+
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
